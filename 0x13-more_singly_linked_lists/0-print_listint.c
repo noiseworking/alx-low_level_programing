75abbaf2a772ef5673e9ff04e8785a6da6b90c8b
noiseworking <noisegamerwork@gmail.com>
2023-05-02 19:38:23 -0700

    0x013-singlydone

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint - Prints all the elements of a listint_t list.
- * @h: list linked of type listint_t.
+ * print_listint -  prints all the elements of a listint_t list.
+ * @h: list to print
  *
- * Return: number of nodes list_t list.
+ * Return: number of elements in the list
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+size_t count = 0;
 
-	while (h)
-	{
-		printf("%d\n", h->n);
-		nodes++;
-		h = h->next;
-	}
-
-	return (nodes);
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
+return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,25 +1,18 @@
-#ifndef MORE_LIST_H_
-#define MORE_LIST_H_
-#include <stdlib.h>
-#include <unistd.h>
-#include <stddef.h>
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
- *
  * @n: integer
- *
  * @next: points to the next node
- *
  * Description: singly linked list node structure
- *
  * for Holberton project
  */
-
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -37,7 +30,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
-int _putchar(char c);
 
 #endif
