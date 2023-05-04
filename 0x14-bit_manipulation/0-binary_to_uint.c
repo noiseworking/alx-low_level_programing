518bcee8cde1d9db18f1fb6a46825f31c64a6001
noiseworking <noisegamerwork@gmail.com>
2023-05-04 15:51:27 -0700

    0x14-bit_done

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "noise.h"
+
+/**
+ * binary_to_uint - converts binary number to unsigned int.
+ * @b: Binary number in string
+ *
+ * Return: The converted number to unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int num;
+
+	num = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
+}
