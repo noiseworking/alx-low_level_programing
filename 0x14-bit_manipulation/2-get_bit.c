9fc70c9eb4c8e10ae6c661363ce7bfab98e8d425
noiseworking <noisegamerwork@gmail.com>
2023-05-04 16:11:21 -0700

    0x14-bit_done

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index.
+ *
+ * @n: check bits number.
+ * @index: position.
+ *
+ * Return: value of the bit index or -1 if error.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+	if (n == 0)
+		return (0);
+	n = n >> index;
+	if ((n & 1) != 0)
+		return (1);
+	return (0);
+}
