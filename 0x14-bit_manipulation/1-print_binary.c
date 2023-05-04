1558a031af50847fcce6cbc8f98834d31fa86b94
noiseworking <noisegamerwork@gmail.com>
2023-05-04 16:05:39 -0700

    0x14-bit_done

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar('0' + (n & 1));
+}
