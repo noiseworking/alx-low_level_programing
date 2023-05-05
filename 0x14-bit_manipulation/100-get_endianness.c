666a296f81680bbda6d08293b78d1d6bbda606fc
noiseworking <noisegamerwork@gmail.com>
2023-05-04 18:57:55 -0700

    0x14-bit_done

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-  * get_endianness checks the endianess
-  *
-  * Return: 0 if big endian, 1 if little endian.
-  *
-  */
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
 int get_endianness(void)
 {
-	int i = 1;
-	char *ch = (char *)&i;
+	unsigned int x;
+	char *c;
 
-	if (*ch)
-		return (1);
-	return (0);
+	x = 1;
+	c = (char *) &x;
+
+	return ((int)*c);
 }
---
+++ 0x14-bit_manipulation/100-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n;
+
+n = get_endianness();
+if (n != 0)
+{
+printf("Little Endian\n");
+}
+else
+{
+printf("Big Endian\n");
+}
+return (0);
+}
