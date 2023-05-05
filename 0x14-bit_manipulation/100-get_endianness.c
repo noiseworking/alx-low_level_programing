d2d1745ee82ce0df4ec0f5c26b8b6cc46967a933
noiseworking <noisegamerwork@gmail.com>
2023-05-04 17:56:06 -0700

    0x14-bit_done

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * get_endianness checks the endianess
+  *
+  * Return: 0 if big endian, 1 if little endian.
+  *
+  */
+int get_endianness(void)
+{
+	int i = 1;
+	char *ch = (char *)&i;
+
+	if (*ch)
+		return (1);
+	return (0);
+}
