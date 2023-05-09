42174a121aae5fa2fd30dc06157239c9c657f7a4
noiseworking <noisegamerwork@gmail.com>
2023-05-08 19:12:17 -0700

    0x15-file_io-done

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,10 @@
-#include "main.h"
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void print_addr(char *ptr);
 void print_type(char *ptr);
